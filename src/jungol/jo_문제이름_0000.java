4ad7c5800fe594a335544a985bc11ca5848307f0
neverte <neverte@naver.com>
2021-02-25 10:45:38 +0900

    기타

--- src/jungol/jo_문제이름_0000.java
+++ src/jungol/jo_문제이름_0000.java
@@ -9,7 +9,7 @@ import java.io.InputStreamReader;
 //냉장고
 //http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1101&sca=99&sfl=wr_hit&stx=1828
 //제출전에 Main으로 바꾸기, file input 지우기, package 지우기
-public class jo_최민수_0000 {
+public class jo_문제이름_0000 {
 
 	public static void main(String[] args) throws IOException {
 		//테스트 입력
