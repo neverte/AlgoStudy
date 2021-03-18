da65b1c35a99ea9c02959b146f5e239db802cd50
ms <ms@192.168.123.100>
2021-03-18 22:35:19 +0900

    SWEA 3289 서로소 집합

--- src/swea/Solution_난이도_0000_문제이름적기_최민수.java
+++ src/swea/Solution_난이도_0000_문제이름적기_최민수.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 // 패키지 날리기
 // class Solution으로 바꾸기
 // 내부 입력 주석처리하기
-public class Solution_난이도_0000_문제이름적기_구미4반_최민수 {
+public class Solution_난이도_0000_문제이름적기_최민수 {
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/swea/input_@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
