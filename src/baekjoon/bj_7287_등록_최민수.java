2bf2f9cd40d10c467f1347793c734be99416e5db
ms <neverte@naver.com>
2021-04-05 21:53:42 +0900

    백준 7287 등록

---
+++ src/baekjoon/bj_7287_등록_최민수.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+//[브론즈 5] 등록
+//https://www.acmicpc.net/problem/7287
+//제출전에 Main으로 바꾸기, file input 지우기, package 지우기
+public class bj_7287_등록_최민수 {
+	public static void main(String[] args) throws Exception {
+		//테스트 입력
+		System.setIn(new FileInputStream("res/baekjoon/bj_input_7287"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		System.out.println(91);
+		System.out.println("neverte");
+		
+		br.close();
+	}
+}
