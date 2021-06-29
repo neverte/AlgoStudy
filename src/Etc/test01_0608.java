9bbc693a59eaacdd565672e76a0c91b7701fcf01
neverte <neverte@naver.com>
2021-06-29 19:49:27 +0900

    vscode test

--- src/Etc/test01_0608.java
+++ src/Etc/test01_0608.java
@@ -6,21 +6,22 @@ import java.io.InputStreamReader;
 
 public class test01_0608 {
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(System.in);
+		// BufferedReader br = new BufferedReader(System.in);
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//가장 많이 등장하는 알파벳과 개수 출력
+
+		// 가장 많이 등장하는 알파벳과 개수 출력
 		int alpha[] = new int[26];
 		char[] word = br.readLine().toCharArray();
 		for (int i = 0; i < word.length; i++) {
 			alpha[word[i] - 65]++;
 		}
-		
+
 		int max = 0;
 		for (int i = 0; i < alpha.length; i++) {
 			max = Math.max(max, alpha[i]);
 		}
 		System.out.println(max);
+		// vscode test
 	}
 }
