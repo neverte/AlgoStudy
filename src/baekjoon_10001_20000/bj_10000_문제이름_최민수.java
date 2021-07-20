6b3ff02408340007948c9e4b88275a5f6d1a255a
neverte <neverte@naver.com>
2021-07-20 22:32:14 +0900

    백준 12869 뮤탈리스크

--- src/baekjoon_10001_20000/bj_10000_문제이름_최민수.java
+++ src/baekjoon_10001_20000/bj_10000_문제이름_최민수.java
@@ -9,10 +9,10 @@ import java.io.OutputStreamWriter;
 //[브론즈2] 나머지
 //https://www.acmicpc.net/problem/0000
 //제출전에 Main으로 바꾸기, file input 지우기, package 지우기
-public class bj_0000_문제이름_최민수 {
+public class bj_10000_문제이름_최민수 {
 	public static void main(String[] args) throws Exception {
 		// 테스트 입력
-		System.setIn(new FileInputStream("res/baekjoon/bj_input_0000"));
+		System.setIn(new FileInputStream("res/baekjoon/bj_input_10000"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
