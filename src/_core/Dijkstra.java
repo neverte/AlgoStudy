0e1e6efd9e8231f8cf8cd689943a2e505a99d0ec
neverte <neverte@naver.com>
2021-08-02 19:40:55 +0900

    오류 제거

--- src/_core/Dijkstra.java
+++ src/_core/Dijkstra.java
@@ -2,7 +2,6 @@ package _core;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -13,13 +12,13 @@ public class Dijkstra {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		System.setIn(new FileInputStream("res/0322_dijkstra_input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int V = Integer.parseInt(br.readLine()); //정점 갯수
-		int start = 0; //출발점
-		int end = V-1; //도착점
-		int[][] adjMatrix = new int[V][V]; //인접 행렬
-		final int INFINITY=Integer.MAX_VALUE;
-		
+
+		int V = Integer.parseInt(br.readLine()); // 정점 갯수
+		int start = 0; // 출발점
+		int end = V - 1; // 도착점
+		int[][] adjMatrix = new int[V][V]; // 인접 행렬
+		final int INFINITY = Integer.MAX_VALUE;
+
 		StringTokenizer st = null;
 		for (int i = 0; i < V; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -27,29 +26,30 @@ public class Dijkstra {
 				adjMatrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		int[] distance = new int[V];
 		boolean[] visited = new boolean[V];
-		
+
 		Arrays.fill(distance, INFINITY);
 		distance[start] = 0;
-		
+
 		for (int i = 0; i < V; i++) {
 			int min = INFINITY;
-			int current = 0; //min 최소 비용에 해당하는 정점 번호
-			//step1: 처리하지 않은 정점 중에 출발지에서 가장 가까운(최소비용) 정점 선택
+			int current = 0; // min 최소 비용에 해당하는 정점 번호
+			// step1: 처리하지 않은 정점 중에 출발지에서 가장 가까운(최소비용) 정점 선택
 			for (int j = 0; j < V; j++) {
-				if(!visited[j] && min > distance[j]) {
+				if (!visited[j] && min > distance[j]) {
 					min = distance[j];
 					current = j;
 				}
 			}
-			visited[current] = true; //선택 정점 방문 처리
-			if(current == end) break; //선택 정점이 도착정점이면 탈출
-			
-			//step2. 선택된 current를 경유지로 하여 아직 처리하지 않은 다른 정점으로의 최소비용 따져본다.
+			visited[current] = true; // 선택 정점 방문 처리
+			if (current == end)
+				break; // 선택 정점이 도착정점이면 탈출
+
+			// step2. 선택된 current를 경유지로 하여 아직 처리하지 않은 다른 정점으로의 최소비용 따져본다.
 			for (int j = 0; j < V; j++) {
-				if(!visited[j] && adjMatrix[current][j] != 0 && distance[j] > min + adjMatrix[current][j]) {
+				if (!visited[j] && adjMatrix[current][j] != 0 && distance[j] > min + adjMatrix[current][j]) {
 					distance[j] = min + adjMatrix[current][j];
 				}
 			}
--- src/programmers/pro_72411_메뉴리뉴얼_최민수.java
+++ src/programmers/pro_72411_메뉴리뉴얼_최민수.java
@@ -1,61 +1,61 @@
-package programmers;
+// package programmers;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+// import java.io.BufferedReader;
+// import java.io.FileInputStream;
+// import java.io.InputStreamReader;
+// import java.util.PriorityQueue;
 
-//[lv 2] 메뉴 리뉴얼
-//https://programmers.co.kr/learn/courses/30/lessons/72411
-//제출전에 Main으로 바꾸기, file input 지우기, package 지우기
-public class pro_72411_메뉴리뉴얼_최민수 {
-	public static void main(String[] args) throws Exception {
-		//테스트 입력
-//		System.setIn(new FileInputStream("res/baekjoon/bj_input_72411"));
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		/*
-		코스요리 메뉴는 최소 2가지 이상의 단품메뉴
-		최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함
-		
-		각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열 orders
-		"스카피"가 추가하고 싶어하는 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열 course
-		"스카피"가 새로 추가하게 될 코스요리의 메뉴 구성을 문자열 형태로 배열에 담아 return 
-		
-		정답은 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사전 순으로 오름차순 정렬해서 return 해주세요.
-		pq에 담는다?
-		
-		System.out.println((int)'A'); //65
-		*/
-//		br.close();
-	}
-	PriorityQueue<String> nominate;
-	
-    public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-				//아이디어 1 적용
+// //[lv 2] 메뉴 리뉴얼
+// //https://programmers.co.kr/learn/courses/30/lessons/72411
+// //제출전에 Main으로 바꾸기, file input 지우기, package 지우기
+// public class pro_72411_메뉴리뉴얼_최민수 {
+// public static void main(String[] args) throws Exception {
+// //테스트 입력
+// // System.setIn(new FileInputStream("res/baekjoon/bj_input_72411"));
+// // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// /*
+// 코스요리 메뉴는 최소 2가지 이상의 단품메뉴
+// 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함
 
-				//1. 부분 문자열을 만들고
-				//2. 허용 가능한지 체크하자.
+// 각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열 orders
+// "스카피"가 추가하고 싶어하는 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열 course
+// "스카피"가 새로 추가하게 될 코스요리의 메뉴 구성을 문자열 형태로 배열에 담아 return
 
-				//1. 부분 문자열 만들기
-				for(int i=0; i<course.length; i++){
-					makeSubset(course[i]);
-	System.
-				}
+// 정답은 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사전 순으로 오름차순 정렬해서 return 해주세요.
+// pq에 담는다?
 
-        return answer;
-    }
+// System.out.println((int)'A'); //65
+// */
+// // br.close();
+// }
+// PriorityQueue<String> nominate;
 
-}
+// public String[] solution(String[] orders, int[] course) {
+// String[] answer = {};
+// //아이디어 1 적용
 
-//아이디어 1: 가능한 모든 부분 문자열을 만들어서 2회 이상 포함되면 생성
-//부분 문자열 생성: 단품메뉴 개수 26, 26의 부분집합 개수  = 2^26 = 10만 * 64 = 640만 정도
-//각 세트메뉴가 가능한지 체크 최대 20가지. 640만 *20 = 1.2억 = 약 1초
+// //1. 부분 문자열을 만들고
+// //2. 허용 가능한지 체크하자.
 
-//2: 각각의 문자를 카운팅해서 1회 이하인 문자는 조합 후보에조차 넣지 않음.
+// //1. 부분 문자열 만들기
+// for(int i=0; i<course.length; i++){
+// makeSubset(course[i]);
+// System.
+// }
 
-//3: 각각의 손님에 대해 메뉴 별로 연산을 한다.
-//예를 들어 1번을 선택 -> A 선택 -> A 주문한 손님 후보군 뽑기 -> B 선택 -> A 후보군에서 B를 뽑은 사람 있는지 선택
-//이런식으로 진행하는데, 갯수는 계속 늘려나가다가 일치하는 후보군이 없으면 해당 갯수를 유지하고 다음 후보로 넘어간다.
-// ABC -> ABF -> ABG -> AC -> 이런식으로 지나간다.
-//저장은 문자열 형태로 만들어서 
+// return answer;
+// }
+
+// }
+
+// //아이디어 1: 가능한 모든 부분 문자열을 만들어서 2회 이상 포함되면 생성
+// //부분 문자열 생성: 단품메뉴 개수 26, 26의 부분집합 개수 = 2^26 = 10만 * 64 = 640만 정도
+// //각 세트메뉴가 가능한지 체크 최대 20가지. 640만 *20 = 1.2억 = 약 1초
+
+// //2: 각각의 문자를 카운팅해서 1회 이하인 문자는 조합 후보에조차 넣지 않음.
+
+// //3: 각각의 손님에 대해 메뉴 별로 연산을 한다.
+// //예를 들어 1번을 선택 -> A 선택 -> A 주문한 손님 후보군 뽑기 -> B 선택 -> A 후보군에서 B를 뽑은 사람 있는지 선택
+// //이런식으로 진행하는데, 갯수는 계속 늘려나가다가 일치하는 후보군이 없으면 해당 갯수를 유지하고 다음 후보로 넘어간다.
+// // ABC -> ABF -> ABG -> AC -> 이런식으로 지나간다.
+// //저장은 문자열 형태로 만들어서
