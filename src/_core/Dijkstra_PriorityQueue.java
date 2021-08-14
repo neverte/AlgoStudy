31fe0185b44aa4d1fc56d6a1f15c4bfecb080b1f
neverte <neverte@naver.com>
2021-08-14 14:52:02 +0900

    ㅇㅇ

--- src/_core/Dijkstra_PriorityQueue.java
+++ src/_core/Dijkstra_PriorityQueue.java
@@ -1,7 +1,11 @@
 package _core;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 /*
 5
@@ -35,57 +39,62 @@ import java.util.*;
 */
 
 public class Dijkstra_PriorityQueue {
-	private static class Node implements Comparable<Node>{
+	private static class Node implements Comparable<Node> {
 		int vertex;
 		int totalDistance;
-		
+
 		public Node(int vertex, int totalDistance) {
 			this.vertex = vertex;
 			this.totalDistance = totalDistance;
 		}
+
 		@Override
 		public int compareTo(Node o) {
-			return Integer.compare(totalDistance,o.totalDistance);
+			return Integer.compare(totalDistance, o.totalDistance);
 		}
 	}
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine().trim());
-		int V=Integer.parseInt(st.nextToken()); //정점 갯수
-		int start=0; //시작점 인덱스
-		int end=V-1; //도착점 인덱스
-		final int INFINITY=Integer.MAX_VALUE;
-		
-		int[][] matrix=new int[V][V];
-		int[] distance=new int[V];
-		boolean[] visited=new boolean[V];
-		
-		for(int i=0; i<V; i++){
-			st=new StringTokenizer(br.readLine().trim()," ");
-			for(int j=0; j<V; j++){
-				matrix[i][j]=Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int V = Integer.parseInt(st.nextToken()); // 정점 갯수
+		int start = 0; // 시작점 인덱스
+		int end = V - 1; // 도착점 인덱스
+		final int INFINITY = Integer.MAX_VALUE;
+
+		int[][] matrix = new int[V][V];
+		int[] distance = new int[V];
+		boolean[] visited = new boolean[V];
+
+		for (int i = 0; i < V; i++) {
+			st = new StringTokenizer(br.readLine().trim(), " ");
+			for (int j = 0; j < V; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		Arrays.fill(distance,INFINITY);
-		distance[start]=0;
-		
-		PriorityQueue<Node> queue=new PriorityQueue<Node>();
-		queue.offer(new Node(start,distance[start]));
-		
-        while(!queue.isEmpty()){
-			//step1: 방문하지 않은 정점들 중 최소가중치의 정점 선택
-        	Node current=queue.poll();
-        	if(visited[current.vertex])continue;
-        	
-			visited[current.vertex]=true; // 선택 정점 방문 처리
-			if(current.vertex==end) break; // 선택 정점이 도착정점이면 탈출.
-			
-			//step2: current정점을 경유지로 하여 갈수 있는 다른 방문하지 않은 정점들에 대한 처리
-			for(int c=0; c<V; ++c){
-				if(!visited[c] && matrix[current.vertex][c]!=0 && distance[c]>current.totalDistance+matrix[current.vertex][c]){
-					                                              distance[c]=current.totalDistance+matrix[current.vertex][c];
-					                                              queue.offer(new Node(c,distance[c]));
+
+		Arrays.fill(distance, INFINITY);
+		distance[start] = 0;
+
+		PriorityQueue<Node> queue = new PriorityQueue<Node>();
+		queue.offer(new Node(start, distance[start]));
+
+		while (!queue.isEmpty()) {
+			// step1: 방문하지 않은 정점들 중 최소가중치의 정점 선택
+			Node current = queue.poll();
+			if (visited[current.vertex])
+				continue;
+
+			visited[current.vertex] = true; // 선택 정점 방문 처리
+			if (current.vertex == end)
+				break; // 선택 정점이 도착정점이면 탈출.
+
+			// step2: current정점을 경유지로 하여 갈수 있는 다른 방문하지 않은 정점들에 대한 처리
+			for (int c = 0; c < V; ++c) {
+				if (!visited[c] && matrix[current.vertex][c] != 0
+						&& distance[c] > current.totalDistance + matrix[current.vertex][c]) {
+					distance[c] = current.totalDistance + matrix[current.vertex][c];
+					queue.offer(new Node(c, distance[c]));
 				}
 			}
 		}
